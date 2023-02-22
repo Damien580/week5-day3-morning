305eaf6993d065235a49172b6b5c38815eceb956
Damien Redd <damiens.redd@gmail.com>
2023-02-22 13:24:38 -0600

    done before lunch

--- chal-pop-growth/popGrowth.js
+++ chal-pop-growth/popGrowth.js
@@ -1,16 +1,16 @@
-let p = 1200
-let p0 = 1000
-let per = .02
-let grow = 50
-let objPop = (p0*per) + grow
-let yrs = 0
+// let p = 1200
+// let p0 = 1000
+// let per = .02
+// let grow = 50
+// let objPop = (p0*per) + grow
+// let yrs = 0
 
-const nbYear = objPop =>{
-  if (objPop <= p){
-    yrs++
-  } else return yrs
-}
-console.log(nbYear())
+// const nbYear = objPop =>{
+//   if (objPop <= p){
+//     yrs++
+//   } else return yrs
+// }
+// console.log(nbYear())
 
 console.log('========================')
 
@@ -21,4 +21,17 @@ function nbYear(p0, percent, aug, p) {
       return i
     }
   }        
-  nbYear(1000, 2, 50, 1200)
+  console.log(nbYear(1000, 2, 50, 1500))
+
+  console.log('=========================')
+
+  const popGrowth = (p0, percent, aug, goal) =>{
+    let years = 0
+    percent /= 100
+    while (p0 < goal){
+        p0 = p0 + (p0 * percent) + aug
+        years++
+    }
+    return years
+  }
+  console.log(popGrowth(1500, 5, 100, 5000))
